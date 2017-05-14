7eb1c23031f9af2374b30a3a09c57e4405245880
proyaschet <amanipatra@gmail.com>
2017-05-14 20:06:42 +0530

    Initial Commit

---
+++ Virtual TOuristsTests/Virtual_TOuristsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Virtual_TOuristsTests.swift
+//  Virtual TOuristsTests
+//
+//  Created by Amarnath Manipatra on 14/05/17.
+//  Copyright © 2017 otd. All rights reserved.
+//
+
+import XCTest
+@testable import Virtual_TOurists
+
+class Virtual_TOuristsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
